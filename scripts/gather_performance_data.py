80c21907e54f397705769c03295614fe120f2b8c
Erik Ylipää <erik.ylipaa@gmail.com>
2020-05-26 09:13:35 +0200

    Gather all performance data, unfiltered

--- scripts/gather_performance_data.py
+++ scripts/gather_performance_data.py
@@ -2,6 +2,7 @@ import argparse
 from collections import defaultdict
 from pathlib import Path
 import json
+import re
 from mltrain import plotting
 import matplotlib.pyplot as plt
 from csv import DictWriter
@@ -10,8 +11,6 @@ def main():
     parser = argparse.ArgumentParser(description="Summarize performance data to a csv ")
     parser.add_argument('experiment_directories', help="Scan these directories for experiments", type=Path, nargs='+')
     parser.add_argument('output', type=Path)
-    parser.add_argument('--performance-metrics', nargs='+', default=["mean_absolute_error"])
-    parser.add_argument('--hyper-parameters', nargs='+', default=['max_depth', 'n_estimators'])
     args = parser.parse_args()
 
     experiments = []
@@ -22,36 +21,34 @@ def main():
                 experiments.append(metadata_file)
 
     data = []
-
+    model_kwarg_names = set()
+    metric_names = set()
     for metadata_file in experiments:
         try:
             with open(metadata_file) as fp:
                 metadata = json.load(fp)
             experiment_data = dict()
             model_kwargs = metadata['model_metadata']['kwargs']
-            for hp_param in args.hyper_parameters:
-                param_value = model_kwargs[hp_param]
-                if param_value is None:
-                    param_value = -1
-                else:
-                    param_value = float(param_value)
-                experiment_data[hp_param] = param_value
+            for kwarg, value in model_kwargs.items():
+                experiment_data[kwarg] = value
+                model_kwarg_names.add(kwarg)
             log_dir = metadata_file.with_name('logs')
-
-            for performance_metric in args.performance_metrics:
-                performance_metric_file = log_dir / 'best_{}.txt'.format(performance_metric)
-                metric_value = None
-                with open(performance_metric_file) as fp:
-                    for line in fp:
-                        it, value = line.strip().split()
-                        metric_value = float(value)
-                if metric_value is not None:
-                    experiment_data[performance_metric] = metric_value
+            for f in log_dir.iterdir():
+                m = re.match('best_(\w+).txt', f.name)
+                if m is not None:
+                    performance_metric = m.group(1)
+                    with open(f) as fp:
+                        for line in fp:
+                            it, value = line.strip().split()
+                            metric_value = float(value)
+                    if metric_value is not None:
+                        metric_names.add(performance_metric)
+                        experiment_data[performance_metric] = metric_value
             data.append(experiment_data)
         except FileNotFoundError:
             continue
 
-    fieldnames = list(args.performance_metrics) + list(args.hyper_parameters)
+    fieldnames = list(sorted(metric_names)) + list(sorted(model_kwarg_names))
     with open(args.output, 'w') as fp:
         csv_writer = DictWriter(fp, fieldnames=fieldnames)
         csv_writer.writeheader()
