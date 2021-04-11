8f6af6a47b89662cc9f9d675a76ebb16ff1444e4
Erik Ylipää <erik.ylipaa@gmail.com>
2021-04-11 11:27:24 +0200

    Some utility fixes

--- mltrain/util.py
+++ mltrain/util.py
@@ -1,8 +1,12 @@
+import json
+from dataclasses import is_dataclass
+import copy
 import datetime
 import importlib.util
 import re
+from collections import Counter
 from pathlib import Path
-from typing import TypeVar, Union, Type
+from typing import TypeVar, Union, Type, Optional, Sequence
 
 
 def timestamp():
@@ -52,18 +56,22 @@ def load_module(module_path: Union[str, Path]):
     return module_from_file
 
 T = TypeVar('T')
-def load_config(training_config_path: Path, config_type: Type[T]) -> T:
+def load_config(training_config_path: Path, config_type: Type[T], default: Optional[Type[T]]=None) -> T:
     """
     Given a file path, load it as a module and return the first matching object of *config_type*
     :param training_config_path: File containing the module to load
     :param config_type: The object type to look for. E.g. a custom object or dataclass instance
+    :param default: If an instance of the desired class could not be found, return this value instead.
     :return: The first found instance of *config_type*. If no instance is found, ValueError is raised
     """
     mod = load_module(training_config_path)
     for k,v in mod.__dict__.items():
         if isinstance(v, config_type):
             return v
-    raise ValueError(f"File {training_config_path} does not contain any attributes of type {config_type}")
+    if default is not None:
+        return default
+    else:
+        raise ValueError(f"File {training_config_path} does not contain any attributes of type {config_type}")
 
 
 def reconstruct_dataclass(str_rep, dc):
@@ -79,3 +87,33 @@ def reconstruct_dataclass(str_rep, dc):
             sanitized = re.sub(sanitizer, replaced, sanitized)
         contents = eval('dict({})'.format(sanitized))
         return contents
+
+
+class JSONEncoder(json.JSONEncoder):
+    """Custom JSONEncoder which tries to encode failed types (like pathlib Paths) as strings, also adds special
+    handling of dataclasses, adding a 'dataclass_name' and 'dataclass_module' field"""
+
+    def default(self, o):
+        if is_dataclass(o):
+            attributes = copy.copy(o.__dict__)
+            attributes['dataclass_name'] = o.__class__.__name__
+            attributes['dataclass_module'] = o.__module__
+            return attributes
+        try:
+            return json.JSONEncoder.default(self, o)
+        except TypeError:
+            return str(o)
+
+def common_prefix(*paths: Sequence[Path]):
+    """Common prefix of given paths"""
+    counter = Counter()
+
+    for path in paths:
+        assert isinstance(path, Path)
+        counter.update([path])
+        counter.update(path.parents)
+
+    try:
+        return sorted((x for x, count in counter.items() if count >= len(paths)), key=lambda x: len(str(x)))[-1]
+    except LookupError as e:
+        raise ValueError('No common prefix found') from e
